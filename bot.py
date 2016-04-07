b76643a888833fdb3d1582b649e3c68726750e88
Robert Lacok <s1206494@ashbury.inf.ed.ac.uk>
2016-04-07 20:51:33 +0100

    linear regression in theano2

--- bot.py
+++ bot.py
@@ -2,4 +2,7 @@ import pyximport, numpy
 pyximport.install(setup_args={"include_dirs":numpy.get_include()}, reload_support=True)
 
 import super_fast_bot as sfb 
-sfb.run_bbox()
+# sfb.run_bbox()
+
+import theano_linear_regression_bot as thb
+thb.run_bbox()
