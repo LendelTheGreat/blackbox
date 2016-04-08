c6216c7eb1f6ac95046ba1b59c37159dff3d9a8d
Robert Lacok <s1206494@ashbury.inf.ed.ac.uk>
2016-04-08 13:49:47 +0100

    memory bot

--- bot.py
+++ bot.py
@@ -4,5 +4,5 @@ pyximport.install(setup_args={"include_dirs":numpy.get_include()}, reload_suppor
 import super_fast_bot as sfb 
 # sfb.run_bbox()
 
-import theano_bot as thb
+import theano_memory_bot_test as thb
 thb.run_bbox()
