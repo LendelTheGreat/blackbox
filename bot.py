781c1482d809e645648b8c2bbf5a9a73b9dc2f46
LendelTheGreat <langer.tomas@yahoo.com>
2016-04-08 13:22:33 +0100

    l1 l2 regularization

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import pyximport, numpy
 pyximport.install(setup_args={"include_dirs":numpy.get_include()}, reload_support=True)
 
-import super_fast_bot as sfb 
-# sfb.run_bbox()
+import linreg_regularization as lrr
+lrr.run_bbox()
 
-import theano_bot as thb
-thb.run_bbox()
+import linreg_test as lrt
+lrt.run_bbox()
