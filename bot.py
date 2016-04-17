cbb66e801156efa5e241d032c68bfda64657e68b
rastarobbie <robo.lacok@gmail.com>
2016-04-17 20:03:00 +0100

    Clean up clutter - delete

--- bot.py
+++ bot.py
@@ -1,13 +1,21 @@
-import pyximport, numpy
-pyximport.install(setup_args={"include_dirs":numpy.get_include()}, reload_support=True)
+# This is what I used for cython
+import pyximport; pyximport.install()
+import setuptools
+import distutils
 
-# import linreg_regularization as lrr
-# lrr.run_bbox()
+# import linreg_update as bot
+# bot.run_bbox()
 
+# import linreg_squared_test as test_bot
+# test_bot.run_bbox()
 
-import theano_memory_bot_test as thb
-thb.run_bbox()
+import linreg_squared_rnn_test as test_bot
+test_bot.run_bbox()
 
-# import linreg_test as lrt
-# lrt.run_bbox()
+# 'coefs_squared.txt'
+# previous train score: 3091
+# previous test score : 2491
 
+# 'coefs_squared_more.txt'
+# train: 3172
+# test : 2479
--- fast_bot.py
+++
@@ -1,6 +0,0 @@
-import pyximport; pyximport.install()
-import setuptools
-import distutils
-
-import theano_memory_time_bot as bot
-bot.run_bbox()
